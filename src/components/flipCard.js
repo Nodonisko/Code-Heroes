e3047d879a9001ab9353bafc0c994d9cf3d64cf1
Daniel Suchy <suchydan@gmail.com>
2020-03-25 23:37:29 +0100

    Fix member card flickering

--- src/components/flipCard.js
+++ src/components/flipCard.js
@@ -1,24 +1,31 @@
-import React, { useState } from "react"
+import React from "react"
 
-const FlipCard = ({ FrontSide, BackSide }) => {
-  const [flipped, setFlip] = useState(false)
-
-  const toggleFlip = () => {
-    setFlip((prev) => !prev)
+class FlipCard extends React.Component {
+  state = {
+    flipped: false,
   }
 
-  return (
-    <div class={`flip-card ${flipped ? "flipped" : ""}`}>
-      <div class="flip-card-inner">
-        <div class="flip-card-front">
-          <FrontSide toggleFlip={toggleFlip} />
-        </div>
-        <div class="flip-card-back">
-          <BackSide toggleFlip={toggleFlip} />
+  toggleFlip = () => {
+    this.setState((prev) => ({ flipped: !prev.flipped }))
+  }
+
+  render() {
+    const { flipped } = this.state
+    const { FrontSide, BackSide } = this.props
+    console.log(this.props, this.state, this.toggleFlip)
+    return (
+      <div class={`flip-card ${flipped ? "flipped" : ""}`}>
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <FrontSide toggleFlip={this.toggleFlip} />
+          </div>
+          <div class="flip-card-back">
+            <BackSide toggleFlip={this.toggleFlip} />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default FlipCard
--- src/components/member.js
+++ src/components/member.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef } from "react"
 import Image from "./image"
 import FlipCard from "./flipCard"
 
@@ -10,74 +10,74 @@ const Member = ({
   avatar,
   links,
   scrollPosition,
-}) => (
-  <div className="col-md-4">
-    <div style={{ position: "relative" }}>
-      <FlipCard
-        FrontSide={({ toggleFlip }) => (
-          <div className="member member-frontside">
-            <div className="member-avatar-container">
-              <div className="member-avatar">
+}) => {
+  const cardRef = useRef()
+
+  const toggle = () => {
+    cardRef.current.toggleFlip()
+  }
+
+  return (
+    <div className="col-md-4">
+      <div style={{ position: "relative" }}>
+        <FlipCard
+          ref={cardRef}
+          FrontSide={() => (
+            <div className="member member-frontside">
+              <div className="member-avatar-container">
+                <div className="member-avatar">
+                  <Image
+                    src={avatar}
+                    alt={name}
+                    scrollPosition={scrollPosition}
+                  />
+                </div>
+              </div>
+              <h3>{name}</h3>
+              <div className="member-desc">
+                {masterSkill} <br></br>
+                {secondarySkill}
+              </div>
+              <div className="flip-arrow" role="button" onClick={toggle}>
                 <Image
-                  src={avatar}
-                  alt={name}
-                  scrollPosition={scrollPosition}
+                  src={require("../images/arrow-right.svg")}
+                  alt="Flip card"
                 />
               </div>
             </div>
-            <h3>{name}</h3>
-            <div className="member-desc">
-              {masterSkill} <br></br>
-              {secondarySkill}
-            </div>
-            <div className="flip-arrow" role="button" onClick={toggleFlip}>
-              <Image
-                src={require("../images/arrow-right.svg")}
-                alt="Flip card"
-              />
-            </div>
-            <div
-              className="flip-hitslop"
-              role="button"
-              onClick={toggleFlip}
-            ></div>
-          </div>
-        )}
-        BackSide={({ toggleFlip }) => (
-          <div className="member member-backside">
-            <h3>{name}</h3>
-            {skills && (
-              <div className="member-desc">
-                <ul>
-                  {skills.map((skill) => (
-                    <li key={skill}>{skill}</li>
-                  ))}
-                </ul>
+          )}
+          BackSide={() => (
+            <div className="member member-backside">
+              <h3>{name}</h3>
+              {skills && (
+                <div className="member-desc">
+                  <ul>
+                    {skills.map((skill) => (
+                      <li key={skill}>{skill}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              <div className="member-links">
+                {links.map(({ href, title }) => (
+                  <a key={title} href={href}>
+                    {title}
+                  </a>
+                ))}
               </div>
-            )}
-            <div className="member-links">
-              {links.map(({ href, title }) => (
-                <a key={title} href={href}>
-                  {title}
-                </a>
-              ))}
+              <div className="flip-arrow" role="button" onClick={toggle}>
+                <Image
+                  src={require("../images/arrow-left.svg")}
+                  alt="Flip card"
+                />
+              </div>
             </div>
-            <div className="flip-arrow" role="button" onClick={toggleFlip}>
-              <Image
-                src={require("../images/arrow-left.svg")}
-                alt="Flip card"
-              />
-            </div>
-            <div
-              className="flip-hitslop"
-              role="button"
-              onClick={toggleFlip}
-            ></div>
-          </div>
-        )}
-      />
+          )}
+        />
+        <div className="flip-hitslop" role="button" onClick={toggle}></div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Member
