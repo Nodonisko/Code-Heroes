7b82fecb99cc787dd74ed1e2431ef5050aba5c79
Jan Václavík <jvaclavik@gmail.com>
2020-03-28 10:45:04 +0100

    Add Hotjar tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-hotjar`,
+      options: {
+        id: 1746429,
+        sv: 6,
+      },
+    },
     `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
