918a4514b9fb5bcdc70903cec4adbeb75937d91f
Daniel Suchy <suchydan@gmail.com>
2020-03-29 17:56:25 +0200

    Add FB thumbnail image

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import favicon from "../images/icon.png"
+import FBThumbnail from "../images/fb-thumbnail.png"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -53,6 +53,10 @@ function SEO({ description, lang, meta, title }) {
           content: `website`,
         },
         {
+          property: `og:image`,
+          content: FBThumbnail,
+        },
+        {
           name: `twitter:card`,
           content: `summary`,
         },
@@ -67,6 +71,10 @@ function SEO({ description, lang, meta, title }) {
         {
           name: `twitter:description`,
           content: metaDescription,
+        },
+        {
+          property: `twitter:image`,
+          content: FBThumbnail,
         },
       ].concat(meta)}
     />
