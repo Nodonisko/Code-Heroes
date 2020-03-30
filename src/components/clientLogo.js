d5b696e942da470c8491f49d0cd301a7bc5e1967
Jan Václavík <jvaclavik@gmail.com>
2020-03-30 19:37:18 +0200

    Minimize image sizes and replace lazy loaded images in Clients

--- src/components/clientLogo.js
+++ src/components/clientLogo.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Image from "./image"
 
 const ClientLogo = (props) => {
   return (
     <div className="col-md-4 col-sm-6">
       <div className="client-logo">
-        <Image {...props} />
+        <img {...props} />
       </div>
     </div>
   )
