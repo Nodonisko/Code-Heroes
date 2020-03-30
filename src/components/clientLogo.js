c1ad83d5477076ceff6a4d7d1de04c6e580ca2cb
Daniel Suchy <suchydan@gmail.com>
2020-03-30 20:29:26 +0200

    Fix lazy load for client images

--- src/components/clientLogo.js
+++ src/components/clientLogo.js
@@ -1,10 +1,17 @@
 import React from "react"
+import Image from "./image"
 
 const ClientLogo = (props) => {
   return (
     <div className="col-md-4 col-sm-6">
       <div className="client-logo">
-        <img {...props} />
+        <Image
+          {...props}
+          style={{
+            height: parseInt(props.height),
+            width: parseInt(props.width),
+          }}
+        />
       </div>
     </div>
   )
